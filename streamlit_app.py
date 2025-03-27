bec8b19ffbd71509f3915ecc0e74bf950493594d
nicolalopomo <nicola.lopomo@polimi.it>
2025-03-27 09:28:47 +0000

    I did some changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My new application for DV4S!")
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+value = st.slider('Select a value: ', 0, 100, 30)
+
+st.write('You selected: ', value)
+
+
